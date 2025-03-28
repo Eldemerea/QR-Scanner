1997f4997072ac60a6eabe9b04a1b3e0b75e9034
Rostyslav Andrusevych <76594620+Eldemerea@users.noreply.github.com>
2025-03-28 15:41:33 +0200

    My first commit here

--- QR Scanner/ContentView.swift
+++ QR Scanner/ContentView.swift
@@ -81,7 +81,7 @@ struct ContentView: View {
                 Button(action: {
                     self.isPresentingScanner = true
                 }) {
-                    Text("Scan QR Code")
+                    Text("Scan QR Code ioioioio")
                         .font(.title2)
                         .frame(maxWidth: .infinity)
                         .padding()
